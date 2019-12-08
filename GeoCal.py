83f0a682b184c419fa5a193d36eaa7b73d1d2222
unknown <62070088@kmitl.ac.th>
2019-12-08 17:19:33 +0700

    Centered the images drawn by Turtle

--- GeoCal.py
+++ GeoCal.py
@@ -22,7 +22,7 @@ h_px = root.winfo_screenheight()
 px_cm = ((w_px / (root.winfo_screenmmwidth() / 10) + h_px / (root.winfo_screenmmheight() / 10)) / 2)
 scrollbar = tk.Scrollbar(root, orient=tk.VERTICAL, activebackground="#00ffff")
 scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH)
-mylist = tk.Listbox(root, yscrollcommand=scrollbar.set, width=w_px//4)
+mylist = tk.Listbox(root, yscrollcommand=scrollbar.set, width=int(w_px*0.10))
 
 def main():
     """Main part of the programme.
@@ -53,6 +53,9 @@ def square(t):
     lenght = 10 if lenght == "" else float(lenght)
     lenghtpx = lenght * px_cm
     lenghtpx = w_px/3 if lenghtpx > w_px//2 else lenghtpx
+    t.penup()
+    t.setpos(-(lenghtpx/2), -(lenghtpx/2))
+    t.pendown()
     polygon(t, 4, lenghtpx)
     return lenght
 
@@ -64,6 +67,9 @@ def recta(t):
     sidepx = [i*px_cm for i in side]
     ratio = sidepx[0] / sidepx[1] #will be used if input is too large to draw
     sidepx = [(w_px/3)*ratio, w_px/3] if any([sidepx[0] > w_px//2, sidepx[1] > w_px//2]) else sidepx
+    t.penup()
+    t.setpos(-(sidepx[0]/2), -(sidepx[1]/2))
+    t.pendown()
     for _ in range(2):
         t.fd(sidepx[0])
         t.lt(90)
@@ -77,28 +83,34 @@ def circle(t):
     radius = turtle.textinput("Please input the radius", "in cm")
     radius = 5 if radius == "" else float(radius)
     radiuspx = radius * px_cm
-    radiuspx = w_px/3 if radiuspx > w_px//2 else radiuspx
+    radiuspx = w_px/6 if radiuspx > w_px//2 else radiuspx
+    t.penup()
+    t.setpos(0, -radiuspx,)
+    t.pendown()
     arc(t, radiuspx)
     return radius
 
 def ellipse(t):
     """Draw an ellipse"""
     radius = turtle.textinput("Please enter the radiuses", "Minor Major\nin cm")
-    radius = [50, 100] if radius == "" else [float(i) for i in radius.split()]
+    radius = [2, 3] if radius == "" else [float(i) for i in radius.split()]
     radiuspx = [i*px_cm for i in radius]
     ratio = radiuspx[0] / radiuspx[1] #will be used if input is too large to draw
-    radiuspx = [(w_px/3)*ratio, w_px/3] if any([radiuspx[0] > w_px//2, radiuspx[1] > w_px//2]) else radiuspx
+    radiuspx = [(w_px/12)*ratio, w_px/12] if any([radiuspx[0]/2 > h_px/4, radiuspx[1]/2 > w_px/4]) else radiuspx
+    t.penup()
+    t.setpos(-(radiuspx[1]), -(radiuspx[0]))
+    t.pendown()
     t.rt(45)
-    arc(t, radiuspx[1], 90)
-    arc(t, radiuspx[0], 90)
-    arc(t, radiuspx[1], 90)
-    arc(t, radiuspx[0], 90)
+    arc(t, radiuspx[1]/3, 90)
+    arc(t, radiuspx[0]/3, 90)
+    arc(t, radiuspx[1]/3, 90)
+    arc(t, radiuspx[0]/3, 90)
     return radius
 
 #Draw the lines
 def arc(t, radius=50, angle=360.0):
     """Draw an arc"""
-    side = int(angle//(radius//10)) + 10
+    side = int(angle/(radius/30)) + 20
     lenght = (pi * radius * 2.0)/side
     angle /= side
     polyline(t, side, lenght, angle)
@@ -124,29 +136,29 @@ def shape_info(name, shape_data):
 
 def info_sq(side):
     """Info of the square"""
-    text = "The area of this square is %.3f sqcm.\n(Side^2)\n"%(side**2)
-    text += "The circumference of this square is %.3f cm\n(Side x 4)\n"%(side*4)
+    text = "The area of this square is %.3f sqcm.  (Side^2)    "%(side**2)
+    text += "The circumference of this square is %.3f cm  (Side x 4)    "%(side*4)
     text += "All four corners of a square are right angles (90°)."
     return text
 
 def info_rect(side):
     """Info of the rectangle"""
-    text = "The area of this rectangle is %.3f sqcm.\n(Width x Height)\n"%(side[0] * side[1])
-    text += "The circumference of this rectangle is %.3f cm.\n(Width x 2 + Height x 2)\n"%(sum(side)*2)
+    text = "The area of this rectangle is %.3f sqcm.  (Width x Height)    "%(side[0] * side[1])
+    text += "The circumference of this rectangle is %.3f cm.  (Width x 2 + Height x 2)    "%(sum(side)*2)
     text += "All four corners of a rectangle are right angles (90°)."
     return text
 
 def info_circ(radius):
     """Infro of the circle"""
-    text = "The area of this circle is %.3f sqcm.\n(π x r^2)\n"%(pi * radius ** 2)
-    text += "The circumference of this circle is %.3f cm.\n(2 x π x r)\n"%(2 * pi * radius)
+    text = "The area of this circle is %.3f sqcm.  (π x r^2)    "%(pi * radius ** 2)
+    text += "The circumference of this circle is %.3f cm.  (2 x π x r)    "%(2 * pi * radius)
     return text
 
 def info_ell(radius):
     """Info of the radius"""
-    text = "The area of this ellipse is %.3f sqcm.\n(π x a x b)\n"%(pi * radius[0] * radius[1])
+    text = "The area of this ellipse is %.3f sqcm.  (π x a x b)    "%(pi * radius[0] * radius[1])
     text += "The approximate circumference of this "
-    text += "ellipse is %.3f cm.\n(2 x π x √((a^2 + b^2)/2))\n"%(2 * pi * sqrt((radius[0]**2 + radius[1]**2)/2))
+    text += "ellipse is %.3f cm.  (2 x π x √((a^2 + b^2)/2))    "%(2 * pi * sqrt((radius[0]**2 + radius[1]**2)/2))
     return text
 
 def repeat():
