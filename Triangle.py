442e29e04d05bdd3d5fcf9576414b54ef9556abf
unknown <62070088@kmitl.ac.th>
2019-12-09 16:29:04 +0700

    Added a turtle drawing, corrected the inputs

--- Triangle.py
+++ Triangle.py
@@ -1,17 +1,31 @@
-from math import *
-import math
+import turtle
+import tkinter as tk
+from math import acos, degrees
+root = tk.Tk()
+screen = turtle.Screen()
+root.title("Geometric Shape Calculator")
+w_px = root.winfo_screenwidth()
+h_px = root.winfo_screenheight()
+#Estimation of the amount of pixel in a 1 cm line in any direction
+px_cm = ((w_px / (root.winfo_screenmmwidth() / 10) + h_px / (root.winfo_screenmmheight() / 10)) / 2)
+
 """Triangle"""
-def main():
+def triangle():
     """Find the angle of the triangle"""
-    angles_a = float(input("Enter the angles for a: "))
-    angles_b = float(input("enter the angles for b: "))
-    angles_c = float(input("enter the angles for c: "))
-    sind_A = degrees(acos((angles_b**2 + angles_c**2 - angles_a**2)/(2*angles_b*angles_c)))
-    sind_B = degrees(acos((angles_c**2 + angles_a**2 - angles_b**2)/(2*angles_c*angles_a)))
-    sind_C = degrees(acos((angles_a**2 + angles_b**2 - angles_c**2)/(2*angles_a*angles_b)))
+    t = turtle.Turtle()
+    side = turtle.textinput("Please input the 3 sides in cm.", "A B C")
+    side = [float(i)*px_cm for i in side.split()]
+    angle_A = degrees(acos((side[1]**2 + side[2]**2 - side[0]**2)/(2*side[1]*side[2])))
+    angle_B = degrees(acos((side[2]**2 + side[0]**2 - side[1]**2)/(2*side[2]*side[0])))
+    angle_C = degrees(acos((side[0]**2 + side[1]**2 - side[2]**2)/(2*side[0]*side[1])))
+    t.fd(side[0])
+    t.lt(180-angle_C)
+    t.fd(side[1])
+    t.lt(180-angle_A)
+    t.fd(side[2])
 
-    print("Angles A is %.2f"%sind_A)
-    print("Angles B is %.2f"%sind_B)
-    print("Angles C is %.2f"%sind_C)
+    print("Angles A is %.2f"%angle_A)
+    print("Angles B is %.2f"%angle_B)
+    print("Angles C is %.2f"%angle_C)
 
-main()
+triangle()
