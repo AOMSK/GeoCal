df287557a663a71b3bc8c02e9d5024c2f22e3ead
unknown <62070088@kmitl.ac.th>
2019-12-09 12:00:22 +0700

    Added realistic angles and the vertical sides

--- parallelogram.py
+++ parallelogram.py
@@ -1,23 +1,28 @@
 """parallelogram"""
 import turtle
-"""pal = turtle.Turtle()
-pal.forward(150)
-pal.right(110)
-pal.forward(90)
-pal.right(70)
-pal.forward(150)
-pal.right(110)
-pal.forward(90)"""
+import tkinter as tk
+from math import asin, degrees
+root = tk.Tk()
+screen = turtle.Screen()
+root.title("Geometric Shape Calculator")
+w_px = root.winfo_screenwidth()
+h_px = root.winfo_screenheight()
+#Estimation of the amount of pixel in a 1 cm line in any direction
+px_cm = ((w_px / (root.winfo_screenmmwidth() / 10) + h_px / (root.winfo_screenmmheight() / 10)) / 2)
 
 def parallelogram():
     """#Draw Parallelogram"""
     t = turtle.Turtle()
-    side = turtle.textinput("Please input the size in cm", "base side")
+    side = turtle.textinput("Please input the size in cm", "base height vertical side")
     side = [float(i) for i in side.split()]
+    side = [i * px_cm for i in side]
+    sine = side[1] / side[2]
+    lesser_angle = degrees(asin(sine))
+    print(lesser_angle)
     for _ in range(2):
         t.forward(side[0])
-        t.right(110)
-        t.forward(side[1])
-        t.right(70)
+        t.lt(180-(180-lesser_angle))
+        t.forward(side[1]*1.5)
+        t.lt(180-lesser_angle)
     return side
 parallelogram()
