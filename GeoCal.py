e5cc7fe8c6506da14fa0e6a8b487f3398dbf1a8e
unknown <62070088@kmitl.ac.th>
2019-12-10 14:32:28 +0700

    GeoCal will now tell the user if the image drawn has been resized

--- GeoCal.py
+++ GeoCal.py
@@ -22,7 +22,8 @@ h_px = root.winfo_screenheight()
 px_cm = ((w_px / (root.winfo_screenmmwidth() / 10) + h_px / (root.winfo_screenmmheight() / 10)) / 2)
 scrollbar = tk.Scrollbar(root, orient=tk.VERTICAL, activebackground="#00ffff")
 scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH)
-mylist = tk.Listbox(root, yscrollcommand=scrollbar.set, width=int(w_px*0.10))
+mylist = tk.Listbox(root, yscrollcommand=scrollbar.set, width=int(w_px/10))
+#root.maxsize(int(w_px//1.5), int(h_px//1.5))
 print("Geometric Calculator")
 print("Shapes currently supported:\nSquare\nRectangles\nCircles\nEllipses")
 print("Shapes too large will be resized to fit the screen but the calculations will still used the input given.")
@@ -40,6 +41,7 @@ def main():
     shapelist = {"square": square, "circle": circle, "rectangle": recta, "ellipse": ellipse}
     try:
         shape = shape.lower()
+        mylist.insert(tk.END, "Shape input: %s"%shape)
         info = shapelist[shape](bob)
         bob.hideturtle()
         shape_info(shape, info)
@@ -55,9 +57,16 @@ def main():
 def square(t):
     """Draw a square"""
     lenght = turtle.textinput("Please input the size", "in cm")
-    lenght = 10 if lenght == "" else float(lenght)
+    if lenght == "":
+        lenght = 10
+        mylist.insert(tk.END, "DIDN'T INPUT THE REQUIRED VALUE(S), USING THE DEFAULT VALUE(S)")
+    else:
+        lenght = float(lenght)
+    mylist.insert(tk.END, str(lenght))
     lenghtpx = lenght * px_cm
-    lenghtpx = w_px/3 if lenghtpx > w_px//2 else lenghtpx
+    if lenghtpx > h_px//2:
+        lenghtpx = h_px/1.2
+        mylist.insert(tk.END, "Input too large: drawing resized")
     t.penup()
     t.setpos(-(lenghtpx/2), -(lenghtpx/2))
     t.pendown()
@@ -67,11 +76,17 @@ def square(t):
 def recta(t):
     """Draw a quadrilateral"""
     side = turtle.textinput("Please input the size in cm", "side1 side2")
-    side = [float(i) for i in side.split()]
-    side = [5, 10] if side == [] else side
+    if side == "":
+        side = [5, 10]
+        mylist.insert(tk.END, "DIDN'T INPUT THE REQUIRED VALUE(S), USING THE DEFAULT VALUE(S)")
+    else:
+        side = [float(i) for i in side.split()]
+    mylist.insert(tk.END, scatter(side))
     sidepx = [i*px_cm for i in side]
     ratio = sidepx[0] / sidepx[1] #will be used if input is too large to draw
-    sidepx = [(w_px/3)*ratio, w_px/3] if any([sidepx[0] > w_px//2, sidepx[1] > w_px//2]) else sidepx
+    if any([sidepx[0] > w_px//2, sidepx[1] > w_px//2]):
+        sidepx = [(w_px/3)*ratio, w_px/3]
+        mylist.insert(tk.END, "Input too large: drawing resized")
     t.penup()
     t.setpos(-(sidepx[0]/2), -(sidepx[1]/2))
     t.pendown()
@@ -86,9 +101,16 @@ def recta(t):
 def circle(t):
     """Draw a circle of a certain radius"""
     radius = turtle.textinput("Please input the radius", "in cm")
-    radius = 5 if radius == "" else float(radius)
+    if radius == "":
+        radius = 5
+        mylist.insert(tk.END, "DIDN'T INPUT THE REQUIRED VALUE(S), USING THE DEFAULT VALUE(S)")
+    else:
+        radius = float(radius)
+    mylist.insert(tk.END, str(radius))
     radiuspx = radius * px_cm
-    radiuspx = w_px/4 if radiuspx > w_px//2 else radiuspx
+    if radiuspx > h_px//2:
+        radiuspx = w_px/4
+        mylist.insert(tk.END, "Input too large: drawing resized")
     t.penup()
     t.setpos(0, -radiuspx,)
     t.pendown()
@@ -98,10 +120,17 @@ def circle(t):
 def ellipse(t):
     """Draw an ellipse"""
     radius = turtle.textinput("Please enter the radiuses", "Minor Major\nin cm")
-    radius = [2, 3] if radius == "" else [float(i) for i in radius.split()]
+    if radius == "":
+        radius = [2, 3]
+        mylist.insert(tk.END, "DIDN'T INPUT THE REQUIRED VALUE(S), USING THE DEFAULT VALUE(S)")
+    else:
+        radius = [float(i) for i in radius.split()]
+    mylist.insert(tk.END, scatter(radius))
     radiuspx = [i*px_cm for i in radius]
-    ratio = radiuspx[0] / radiuspx[1] #will be used if input is too large to draw
-    radiuspx = [(w_px/6)*ratio, w_px/6] if any([radiuspx[0]/2 > h_px/4, radiuspx[1]/2 > w_px/4]) else radiuspx
+    if any([radiuspx[0]/2 > h_px/4, radiuspx[1]/2 > w_px/4]):
+        ratio = radiuspx[0] / radiuspx[1] #will be used if input is too large to draw
+        radiuspx = [(w_px/6)*ratio, w_px/6]
+        mylist.insert(tk.END, "Input too large: drawing resized")
     t.penup()
     t.setpos(-(radiuspx[1]), -(radiuspx[0]))
     t.pendown()
@@ -165,6 +194,10 @@ def info_ell(radius):
     text += "The approximate circumference of this "
     text += "ellipse is %.3f cm.  (2 x π x √((a^2 + b^2)/2))    "%(2 * pi * sqrt((radius[0]**2 + radius[1]**2)/2))
     return text
+
+def scatter(input_list):
+    """Scatter the inputs into a string"""
+    return " ".join([str(i) for i in input_list])
 
 def repeat():
     """Run this code again"""
