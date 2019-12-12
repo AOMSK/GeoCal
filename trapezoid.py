6fc012bce79e8df7bb5508a44b97c70c623e4012
R0C0KY <50584701+R0C0KY@users.noreply.github.com>
2019-12-12 13:30:50 +0700

    Update trapezoid.py

--- trapezoid.py
+++ trapezoid.py
@@ -4,12 +4,13 @@ from math import asin, acos, degrees
 def main():
     """Draw trapezoid"""
     #enter trapezoid parts
-
-    base_a = float(input("Enter the base A length: "))
-    base_b = float(input("Enter the base B length: "))
-    leg_a = float(input("Enter the leg A length: "))
-    leg_b = float(input("Enter the legB length: "))
-    height = float(input("Enter the height: "))
+    size = turtle.textinput("Enter the sizes:", "Base_A Base_B Leg_A Leg_B Height")
+    size = [float(i) for i in size.split()]
+    base_a = size[0] * px_cm
+    base_b = size[1] * px_cm
+    leg_a = size[2] * px_cm
+    leg_b = size[3] * px_cm
+    height = size[4] * px_cm
     #find base angles
     angles_1 = math.degrees(math.asin(height/leg_a))
     angles_2 = math.degrees(math.acos(height/leg_b))
@@ -24,5 +25,11 @@ def main():
     t.fd(min(base_a, base_b))
     t.lt(90-angles_2)
     t.fd(leg_b)
+
+def info_trap(size, angles_1, angles_2):
+    """Info of the trapezoid"""
+    text = "The area of this trapezoid is %.2f sqcm. (1/2 x (Base_A+Base_B) x Height)|"%(1/2*(size[0]+size[1])*size[4])
+    text += "The Perimeter of this trapezoid is %.2f cm. (Base_A+Base_B+Leg_A+Leg_B)"%(size[0]+size[1]+size[2]+size[3])
+    text += "All angles in this trapezoid is %0.2f, %0.2f, %0.2f, %0.2f."%(angles_1, 180-angles_1, angles_2+90, 180-(angles_2+90))
 
 main()
