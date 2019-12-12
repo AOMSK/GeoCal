b681845b57bfab7e800d35220638f63ac8abc3da
R0C0KY <50584701+R0C0KY@users.noreply.github.com>
2019-12-12 14:14:59 +0700

    Update trapezoid.py

--- trapezoid.py
+++ trapezoid.py
@@ -9,7 +9,7 @@ h_px = root.winfo_screenheight()
 #Estimation of the amount of pixel in a 1 cm line in any direction
 px_cm = ((w_px / (root.winfo_screenmmwidth() / 10) + h_px / (root.winfo_screenmmheight() / 10)) / 2)
 """Trapezoid"""
-def main():
+def trapezoid(t):
     """Draw trapezoid"""
     #enter trapezoid parts
     size = turtle.textinput("Enter the sizes:", "Base_A Base_B Leg_A Leg_B Height")
@@ -23,7 +23,6 @@ def main():
     angles_1 = degrees(asin(height/leg_a))
     angles_2 = degrees(acos(height/leg_b))
     #Turtle
-    t = turtle.Turtle()
     t.fd(max(base_a, base_b))
     t.lt(90+(90-angles_1))
     t.fd(leg_a)
@@ -32,9 +31,11 @@ def main():
     t.lt(90-angles_2)
     t.fd(leg_b)
     #Info for trapezoid
+    return size + [angles_1, angles_2]
+
+def info_trap(size)
+    """Info of the trapezoid"""
     text = "The area of this trapezoid is %0.2f sqcm."%(1/2*(size[0]+size[1])*size[4])
     text += "The Perimeter of this trapezoid is %0.2f cm."%(size[0]+size[1]+size[2]+size[3])
-    text += "All angles in this trapezoid is %0.2f, %0.2f, %0.2f, %0.2f."%(angles_1, 180-angles_1, angles_2+90, 180-(angles_2+90))
-    print(text)
-
+    text += "All angles in this trapezoid is %0.2f, %0.2f, %0.2f, %0.2f."%(size[5], 180-size[5], size[6]+90, 180-(size[6]+90))
 main()
